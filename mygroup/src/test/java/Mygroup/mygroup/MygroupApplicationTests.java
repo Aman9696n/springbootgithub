5036b8d051c097be37d2f8ea4bea62ccdeb1d0bc
Aman9696n <amanmishra9696@gmail.com>
2023-03-31 10:59:20 +0000

    unziped project file

---
+++ mygroup/src/test/java/Mygroup/mygroup/MygroupApplicationTests.java
@@ -0,0 +1,13 @@
+package Mygroup.mygroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
